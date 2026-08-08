3d0cb6a1531ca1d16ed54c18b9af8bc3593b2de0
syb999 <13062678135@163.com>
2026-08-08 09:30:12 +0800

    luci: add logo

--- modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/40_dhcp.js
+++ modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/40_dhcp.js
@@ -17,7 +17,212 @@ return baseclass.extend({
 	isMACStatic: {},
 	isDUIDStatic: {},
 
+	macPrefixToDevice: {},
+
+	deviceIcons: {
+		"mi": "/luci-static/resources/icons/devices/mi.png",
+		"apple": "/luci-static/resources/icons/devices/apple.png",
+		"huawei": "/luci-static/resources/icons/devices/huawei.png",
+		"honor": "/luci-static/resources/icons/devices/honor.png",
+		"oppo": "/luci-static/resources/icons/devices/oppo.png",
+		"realme": "/luci-static/resources/icons/devices/realme.png",
+		"vivo": "/luci-static/resources/icons/devices/vivo.png",
+		"lenovo": "/luci-static/resources/icons/devices/lenovo.png",
+		"orangepi": "/luci-static/resources/icons/devices/orangepi.png",
+		"dell": "/luci-static/resources/icons/devices/dell.png",
+		"iqoo": "/luci-static/resources/icons/devices/iqoo.png",
+		"mijia": "/luci-static/resources/icons/devices/mijia.png",
+		"tmall": "/luci-static/resources/icons/devices/tmall.png",
+		"xiaotiancai": "/luci-static/resources/icons/devices/xiaotiancai.png",
+		"router": "/luci-static/resources/icons/devices/router.png",
+		"tv": "/luci-static/resources/icons/devices/tv.png",
+		"motorola": "/luci-static/resources/icons/devices/motorola.png",
+		"panel_ap": "/luci-static/resources/icons/devices/panel_ap.png",
+		"voip_fxs": "/luci-static/resources/icons/devices/voip_fxs.png",
+		"printer": "/luci-static/resources/icons/devices/printer.png",
+		"iptv": "/luci-static/resources/icons/devices/iptv.png",
+		"nas": "/luci-static/resources/icons/devices/nas.png",
+		"midea": "/luci-static/resources/icons/devices/midea.png",
+		"default": "/luci-static/resources/icons/devices/pc.png"
+	},
+
+	initMacPrefixMap: function() {
+		var map = this.macPrefixToDevice;
+
+		["10:3f:44", "2c:d0:66", "58:20:59", "64:a2:00", "64:cc:2e", 
+		 "8e:49:53", "98:f6:21", "a4:50:46", "b6:3c:d2", "ea:ea:9d",
+		 "ec:41:18", "e8:ca:c8", "f4:f5:db"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "mi";
+		});
+
+		["00:5b:94", "34:a8:eb", "50:82:d5", "64:20:0c", "7a:51:e7", 
+		 "80:be:05", "ae:0b:09", "b8:c1:11", "e2:c1:63", "dc:a9:04"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "apple";
+		});
+
+		["02:5f:84", "02:f8:ea", "04:4f:4c", "12:65:e3", "18:3c:b7", 
+		 "22:17:16", "22:35:b4", "22:df:c6", "24:31:54", "30:a2:c2", 
+		 "32:bc:0e", "3c:cd:5d", "50:01:d9", "62:7f:f0", "6a:fc:40", 
+		 "74:59:09", "7c:a1:77", "82:51:80", "82:a0:d3", "82:a5:6b", 
+		 "82:ef:22", "90:2b:d2", "96:c9:cb", "9c:b2:b2", "a2:65:69", 
+		 "a8:e5:44", "b2:3d:8c", "c2:56:e7", "c2:98:4b", "c2:d0:55", 
+		 "d2:7e:7b", "dc:16:b2", "e2:08:88", "f2:49:84", "f2:6f:80", 
+		 "f2:f9:db"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "huawei";
+		});
+
+		["22:f2:24", "32:5e:86", "a2:84:d5", "b2:8b:c8", "e4:a7:c5"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "honor";
+		});
+
+		["08:4a:cf", "60:21:01", "0a:b1:0e", "3a:01:c5", "ac:c4:bd",
+		 "b4:20:5b"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "oppo";
+		});
+
+
+		["d0:5a:fd"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "realme";
+		});
+
+		["2a:da:93"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "vivo";
+		});
+
+		["ac:38:70"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "lenovo";
+		});
+
+		["02:81:eb"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "orangepi";
+		});
+
+		["00:22:5f"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "dell";
+		});
+
+		["3e:54:f7", "8e:a3:3b", "be:22:ff", "f6:c2:4b"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "iqoo";
+		});
+
+		["64:90:c1", "5c:e5:0c", "44:23:7c"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "mijia";
+		});
+
+		["84:44:af", "8c:6d:50", "78:da:07"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "tmall";
+		});
+
+		["e0:76:d0"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "xiaotiancai";
+		});
+
+		["04:5f:a7", "20:76:93", "38:83:45", "3c:40:4f", "78:a3:51", 
+		 "78:d3:8d", "88:c3:97", "cc:08:fb", "d4:ee:07", "dc:d8:7c"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "router";
+		});
+
+		["2c:e0:32", "6c:22:1a"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "tv";
+		});
+
+		["50:2f:bb", "16:0a:de"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "motorola";
+		});
+
+		["74:96:37", "8a:0c:ca"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "panel_ap";
+		});
+
+		["00:21:f2"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "voip_fxs";
+		});
+
+		["9c:ae:d3"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "printer";
+		});
+
+		["00:07:63", "00:ac:1f", "04:95:73", "0c:56:5c", "30:50:fd", 
+		 "74:c9:a3", "74:ff:4c", "78:53:0d", "90:d8:f3", "ac:00:d0", 
+		 "c8:64:c7", "f4:4c:70"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "iptv";
+		});
+
+		["00:11:32"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "nas";
+		});
+
+		["f0:c9:d1"].forEach(function(prefix) {
+			map[prefix.toLowerCase()] = "midea";
+		});
+	},
+
+	getDeviceIcon: function(macAddr) {
+		if (!macAddr || macAddr.length < 8) {
+			return E('img', {
+				'src': this.deviceIcons.default,
+				'title': _('Unknown device'),
+				'style': 'width: 24px; height: 24px; vertical-align: middle;'
+			});
+		}
+		
+		var macPrefix = macAddr.substring(0, 8).toLowerCase();
+		var deviceType = this.macPrefixToDevice[macPrefix] || "default";
+		var icon = this.deviceIcons[deviceType] || this.deviceIcons.default;
+		
+		return E('img', {
+			'src': icon,
+			'title': this.getDeviceTitle(deviceType),
+			'style': 'width: 24px; height: 24px; vertical-align: middle;'
+		});
+	},
+
+	getDeviceTitle: function(deviceType) {
+		var titles = {
+			"mi": _("Xiaomi"),
+			"apple": _("Apple"),
+			"huawei": _("Huawei"),
+			"honor": _("Honor"),
+			"oppo": _("OPPO"),
+			"realme": _("realme"),
+			"vivo": _("vivo"),
+			"lenovo": _("Lenovo"),
+			"orangepi": _("Orange Pi"),
+			"dell": _("Dell"),
+			"iqoo": _("iQOO"),
+			"mijia": _("Mi Home"),
+			"tmall": _("Tmall Genie"),
+			"xiaotiancai": _("Xiaotiancai"),
+			"router": _("Router"),
+			"tv": _("TV"),
+			"motorola": _("Motorola"),
+			"panel_ap": _("Panel AP"),
+			"voip_fxs": _("VoIP FXS"),
+			"printer": _("Printer"),
+			"iptv": _("IPTV"),
+			"nas": _("NAS"),
+			"midea": _("Midea"),
+			"default": _("PC/Other")
+		};
+		
+		return titles[deviceType] || titles["default"];
+	},
+
+	duidToMac: function(duid) {
+		if (!duid) return null;
+
+		if (duid.length === 28 && duid.substr(0, 8) === '00010001')
+			return duid.substr(16).replace(/(..)(?=..)/g, '$1:').toUpperCase();
+
+		if (duid.length === 24 && duid.substr(0, 8) === '00030001')
+			return duid.substr(8).replace(/(..)(?=..)/g, '$1:').toUpperCase();
+
+		return null;
+	},
+
 	load: function() {
+		this.initMacPrefixMap();
+
 		return Promise.all([
 			callLuciDHCPLeases(),
 			network.getHostHints(),
@@ -84,6 +289,7 @@ return baseclass.extend({
 
 		var table = E('table', { 'id': 'status_leases', 'class': 'table lases' }, [
 			E('tr', { 'class': 'tr table-titles' }, [
+				E('th', { 'class': 'th' }, _('Logo')),
 				E('th', { 'class': 'th' }, _('Hostname')),
 				E('th', { 'class': 'th' }, _('IPv4 address')),
 				E('th', { 'class': 'th' }, _('MAC address')),
@@ -111,9 +317,10 @@ return baseclass.extend({
 				host = lease.hostname;
 
 			rows = [
+				this.getDeviceIcon(lease.macaddr),
 				'%h'.format(host || '-'),
 				lease.ipaddr,
-				lease.macaddr,
+				E('code', { 'style': 'font-family: monospace;' }, lease.macaddr),
 				exp
 			];
 
@@ -131,6 +338,7 @@ return baseclass.extend({
 
 		var table6 = E('table', { 'id': 'status_leases6', 'class': 'table leases6' }, [
 			E('tr', { 'class': 'tr table-titles' }, [
+				E('th', { 'class': 'th' }, _('Logo')),
 				E('th', { 'class': 'th' }, _('Host')),
 				E('th', { 'class': 'th' }, _('IPv6 address')),
 				E('th', { 'class': 'th' }, _('DUID')),
@@ -159,10 +367,14 @@ return baseclass.extend({
 			else if (hint)
 				host = hint[1];
 
+			var macFromDuid = this.duidToMac(lease.duid);
+			var icon = macFromDuid ? this.getDeviceIcon(macFromDuid) : this.getDeviceIcon(lease.macaddr);
+
 			rows = [
+				icon,
 				'%h'.format(host || '-'),
 				lease.ip6addrs ? lease.ip6addrs.join('<br />') : lease.ip6addr,
-				lease.duid,
+				E('code', { 'style': 'font-family: monospace; word-break: break-all;' }, lease.duid),
 				exp
 			];
 
